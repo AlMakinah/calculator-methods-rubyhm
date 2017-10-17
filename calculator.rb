271ca3be50a6e992b735e05b57c69e89d6a4ee52
Hassan Mostafa <hassanmostafa545@gmail.com>
2017-10-17 18:03:15 +0200

    make calculator

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,39 @@
+puts "enter first number"
+x = gets.to_i
+puts "enter operation"
+operator = gets.chomp
+puts "enter second number"
+y = gets.to_i
+def multiply(x, y)
+	return x * y	
+end
+
+def divide(x, y)
+	return x / y	
+end
+
+def subtraction(x, y)
+	return x - y	
+end
+
+def add(x, y)
+	return x + y	
+end
+
+def operation(x, y, operator)
+	case operator
+	when '*'
+		puts multiply(x, y)
+	when '/'
+		puts divide(x, y)
+	when '-'
+		puts subtraction(x, y)
+	when '+'
+		puts add(x, y)													
+	end
+end
+operation(x, y, operator)
+
+
+
 
